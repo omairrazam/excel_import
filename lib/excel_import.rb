f53e66bbc2ce8094b1e27c49230797ae0d69cc2a
omairazam <omsolutionpk@gmail.com>
2019-10-07 04:20:50 +0500

    saving changes

--- lib/excel_import.rb
+++ lib/excel_import.rb
@@ -1,5 +1,5 @@
 module ExcelImport
-	Dir["excel_import/*.rb"].each {|file| require file }
-	Dir["excel_import/adapters/*.rb"].each {|file| require file }
-	Dir["excel_import/extractors/*.rb"].each {|file| require file }
+	Dir[File.dirname(__FILE__) + "/excel_import/*.rb"].each {|file| require file }
+	Dir[File.dirname(__FILE__) + "/excel_import/adapters/*.rb"].each {|file| require file }
+	Dir[File.dirname(__FILE__) + "/excel_import/extractors/*.rb"].each {|file| require file }
 end
